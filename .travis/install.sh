50e00168187b742f91dce5ba7f631f92c1b2ea47
JWKennington <jimmy.kennington@gmail.com>
2020-05-06 23:59:56 -0500

    wip ci

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,42 +6,28 @@ echo "TOXENV = $TOXENV"
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
-    # Install miniconda on macOS
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
-    bash miniconda.sh -b -p $HOME/miniconda
-    source "$HOME/miniconda/etc/profile.d/conda.sh"
-    hash -r
-    conda config --set always_yes yes --set changeps1 no
-    conda update -q conda
+  # Install miniconda on macOS
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+  bash miniconda.sh -b -p $HOME/miniconda
+  source "$HOME/miniconda/etc/profile.d/conda.sh"
+  hash -r
+  conda config --set always_yes yes --set changeps1 no
+  conda update -q conda
 
-    # Useful for debugging any issues with conda
-    conda info -a
+  # Useful for debugging any issues with conda
+  conda info -a
 
 else
 
-    # Install miniconda on Linux
-    sudo apt-get update
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-    bash miniconda.sh -b -p $HOME/miniconda
-    source "$HOME/miniconda/etc/profile.d/conda.sh"
-    hash -r
-    conda config --set always_yes yes --set changeps1 no
-    conda update -q conda
+  # Install miniconda on Linux
+  sudo apt-get update
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+  bash miniconda.sh -b -p $HOME/miniconda
+  source "$HOME/miniconda/etc/profile.d/conda.sh"
+  hash -r
+  conda config --set always_yes yes --set changeps1 no
+  conda update -q conda
 
-    # Useful for debugging any issues with conda
-    conda info -a
+  # Useful for debugging any issues with conda
+  conda info -a
 fi
-
-case "${TOXENV}" in
-    py36)
-        # Install Mac Python3.6 environment
-        conda env create -f environment-36.yml
-        ;;
-    py37)
-        # Install some custom Python 3.3 requirements on macOS
-        conda env create -f environment.yml
-        ;;
-esac
-
-conda activate eark-env
-python setup.py install
