5929711e078513dd2a66b3456ade93c28a6b82fd
JWKennington <jimmy.kennington@gmail.com>
2020-05-07 01:25:58 -0500

    wip ci

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,7 +12,7 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
   # Install miniconda on macOS
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
   bash miniconda.sh -b -p $HOME/miniconda
-  source "$HOME/miniconda/etc/profile.d/conda.sh"
+  . "$HOME/miniconda/etc/profile.d/conda.sh"
   hash -r
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
@@ -26,7 +26,7 @@ else
   sudo apt-get update
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   bash miniconda.sh -b -p $HOME/miniconda
-  source "$HOME/miniconda/etc/profile.d/conda.sh"
+  . "$HOME/miniconda/etc/profile.d/conda.sh"
   hash -r
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
